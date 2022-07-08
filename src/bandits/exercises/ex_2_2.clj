a1b5a8bcbe47a7cc80e4f2cc68e64452767ec425
Nicholas Cellino <nacellin@buffalo.edu>
2022-07-08 00:03:23 -0400

    combined 2 plots into 1

--- src/bandits/exercises/ex_2_2.clj
+++ src/bandits/exercises/ex_2_2.clj
@@ -2,20 +2,22 @@
   (:require
    [bandits.agents :as agents]
    [bandits.testbed :as testbed]
-   [bandits.visualize :as vis]))
+   [bandits.visualize :as vis]
+   [oz.core :as oz]))
 
 (defn run-exp
-  [agents chart-title]
+  [agents]
   (let [results (testbed/run-testbed agents 10 2000 1000)
         labels (map agents/get-plot-label agents)
         labeled-results (vis/label (:rewards results) labels)
-        labeled-optimal-choice-results (vis/label (:optimal-choices results) labels)]
-    (println labeled-optimal-choice-results)
-    (vis/plot-lines labeled-optimal-choice-results ["step" "percentage"] {:title "Optimal choices"})
-    (vis/plot-lines labeled-results ["step" "value"] {:title chart-title})))
+        labeled-optimal-choice-results (vis/label (:optimal-choices results) labels)
+        optimal-choice-plot (vis/create-line-plot labeled-optimal-choice-results ["step" "percentage"] "Optimal choices")
+        rewards-plot (vis/create-line-plot labeled-results ["step" "value"] "Rewards")
+        overall-plot {:vconcat [optimal-choice-plot rewards-plot]}]
+      (oz/view! overall-plot)))
 
 (comment
   "Running experiment 2.2"
   (let [agents (map #(agents/epsilon-greedy-agent % 10) [0 0.01 0.1])]
-    (run-exp agents "Epsilon greedy agents")))
+    (run-exp agents)))
   
